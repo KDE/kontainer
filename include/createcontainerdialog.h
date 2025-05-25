29c3a7d9a0740184f28f0a730bfea25d1b188a58
Hadi Chokr <hadichokr@icloud.com>
2025-05-25 16:33:03 +0200

    Progress Dialog added.

--- include/createcontainerdialog.h
+++ include/createcontainerdialog.h
@@ -2,18 +2,21 @@
 #define CREATECONTAINERDIALOG_H
 
 #include <QDialog>
-#include <QLineEdit>
-#include <QCheckBox>
-#include <QListWidget>
-#include <QFormLayout>
+#include <QProgressDialog>
+#include <QProcess>
 
 class Backend;
+class QLineEdit;
+class QListWidget;
+class QCheckBox;
 
 class CreateContainerDialog : public QDialog
 {
     Q_OBJECT
+
 public:
     explicit CreateContainerDialog(Backend *backend, QWidget *parent = nullptr);
+    ~CreateContainerDialog();
 
     QString containerName() const;
     QString imageUrl() const;
@@ -24,15 +27,21 @@ public:
 private slots:
     void refreshImages();
     void searchImages(const QString &query);
+    void startContainerCreation();
+    void handleCreateFinished(int exitCode, QProcess::ExitStatus exitStatus);
+    void handleReadyRead();
+    void handleErrorOccurred(QProcess::ProcessError error);
 
 private:
     Backend *m_backend;
     QLineEdit *m_nameEdit;
+    QLineEdit *m_searchEdit;
+    QListWidget *m_imageList;
     QLineEdit *m_homeEdit;
     QLineEdit *m_volumesEdit;
     QCheckBox *m_initCheckbox;
-    QLineEdit *m_searchEdit;
-    QListWidget *m_imageList;
+    QProgressDialog *m_progressDialog;
+    QProcess *m_createProcess;
 };
 
 #endif // CREATECONTAINERDIALOG_H
--- src/createcontainerdialog.cpp
+++ src/createcontainerdialog.cpp
@@ -6,13 +6,18 @@
 #include <QPushButton>
 #include <QLabel>
 #include <QMessageBox>
+#include <QListWidgetItem>
+#include <QDebug>
+#include <QLineEdit>
+#include <QCheckBox>
 
 CreateContainerDialog::CreateContainerDialog(Backend *backend, QWidget *parent)
-: QDialog(parent), m_backend(backend)
+: QDialog(parent), m_backend(backend), m_progressDialog(nullptr), m_createProcess(nullptr)
 {
     setWindowTitle("Create New Container");
     resize(600, 400);
 
+    // Setup form layout
     QFormLayout *formLayout = new QFormLayout;
 
     m_nameEdit = new QLineEdit(this);
@@ -31,29 +36,45 @@ CreateContainerDialog::CreateContainerDialog(Backend *backend, QWidget *parent)
     formLayout->addRow("Custom Home Path:", m_homeEdit);
 
     m_volumesEdit = new QLineEdit(this);
-    formLayout->addRow("Additional Volumes (comma separated):", m_volumesEdit);
+    m_volumesEdit->setPlaceholderText("e.g., /host/path:/container/path");
+    formLayout->addRow("Additional Volumes:", m_volumesEdit);
 
     m_initCheckbox = new QCheckBox("Enable systemd init", this);
     formLayout->addRow(m_initCheckbox);
 
+    // Setup buttons
     QHBoxLayout *buttonLayout = new QHBoxLayout;
     QPushButton *refreshButton = new QPushButton("Refresh Images", this);
     connect(refreshButton, &QPushButton::clicked, this, &CreateContainerDialog::refreshImages);
     buttonLayout->addWidget(refreshButton);
 
     QPushButton *createButton = new QPushButton("Create", this);
-    connect(createButton, &QPushButton::clicked, this, &QDialog::accept);
+    createButton->setDefault(true);
+    connect(createButton, &QPushButton::clicked, this, &CreateContainerDialog::startContainerCreation);
     buttonLayout->addWidget(createButton);
 
     QPushButton *cancelButton = new QPushButton("Cancel", this);
     connect(cancelButton, &QPushButton::clicked, this, &QDialog::reject);
     buttonLayout->addWidget(cancelButton);
 
+    // Main layout
     QVBoxLayout *mainLayout = new QVBoxLayout(this);
     mainLayout->addLayout(formLayout);
     mainLayout->addLayout(buttonLayout);
 
     refreshImages();
+}
+
+CreateContainerDialog::~CreateContainerDialog()
+{
+    if (m_createProcess) {
+        if (m_createProcess->state() == QProcess::Running) {
+            m_createProcess->terminate();
+            m_createProcess->waitForFinished(1000);
+        }
+        delete m_createProcess;
+    }
+    delete m_progressDialog;
 }
 
 void CreateContainerDialog::refreshImages()
@@ -63,8 +84,10 @@ void CreateContainerDialog::refreshImages()
 
     for (const auto &image : images) {
         QString url = image["url"];
-        QListWidgetItem *item = new QListWidgetItem(url, m_imageList);
+        QString name = image["name"];
+        QListWidgetItem *item = new QListWidgetItem(name.isEmpty() ? url : name, m_imageList);
         item->setData(Qt::UserRole, url);
+        item->setToolTip(url);
     }
 }
 
@@ -80,12 +103,125 @@ void CreateContainerDialog::searchImages(const QString &query)
 
     for (const auto &image : images) {
         QString url = image["url"];
-        QListWidgetItem *item = new QListWidgetItem(url, m_imageList);
+        QString name = image["name"];
+        QListWidgetItem *item = new QListWidgetItem(name.isEmpty() ? url : name, m_imageList);
         item->setData(Qt::UserRole, url);
+        item->setToolTip(url);
     }
 }
 
-QString CreateContainerDialog::containerName() const { return m_nameEdit->text(); }
+void CreateContainerDialog::startContainerCreation()
+{
+    QString name = containerName();
+    QString image = imageUrl();
+    QString home = homePath();
+    bool init = useInit();
+    QStringList volumes = this->volumes();
+
+    // Validate inputs
+    if (name.isEmpty()) {
+        QMessageBox::warning(this, "Error", "Please enter a container name");
+        return;
+    }
+
+    if (image.isEmpty()) {
+        QMessageBox::warning(this, "Error", "Please select an image");
+        return;
+    }
+
+    // Setup progress dialog
+    m_progressDialog = new QProgressDialog(this);
+    m_progressDialog->setWindowTitle("Creating Container");
+    m_progressDialog->setLabelText("Starting container creation...");
+    m_progressDialog->setRange(0, 0); // Indeterminate progress
+    m_progressDialog->setCancelButton(nullptr);
+    m_progressDialog->setModal(true);
+    m_progressDialog->show();
+
+    // Setup process
+    m_createProcess = new QProcess(this);
+    m_createProcess->setProcessChannelMode(QProcess::MergedChannels);
+
+    connect(m_createProcess, QOverload<int, QProcess::ExitStatus>::of(&QProcess::finished),
+            this, &CreateContainerDialog::handleCreateFinished);
+    connect(m_createProcess, &QProcess::readyRead,
+            this, &CreateContainerDialog::handleReadyRead);
+    connect(m_createProcess, &QProcess::errorOccurred,
+            this, &CreateContainerDialog::handleErrorOccurred);
+
+    // Build command
+    QStringList args = {"create", "-n", name, "-i", image, "-Y"};
+    if (init) {
+        args << "--init" << "--additional-packages" << "systemd";
+    }
+    if (!home.isEmpty()) {
+        args << "--home" << home;
+    }
+    for (const QString &volume : volumes) {
+        if (!volume.trimmed().isEmpty()) {
+            args << "--volume" << volume.trimmed();
+        }
+    }
+
+    qDebug() << "Executing:" << "distrobox" << args;
+    m_createProcess->start("distrobox", args);
+}
+
+void CreateContainerDialog::handleReadyRead()
+{
+    QString output = QString::fromUtf8(m_createProcess->readAll());
+    if (!output.isEmpty()) {
+        m_progressDialog->setLabelText(output);
+        qDebug() << "Process output:" << output;
+    }
+}
+
+void CreateContainerDialog::handleErrorOccurred(QProcess::ProcessError error)
+{
+    m_progressDialog->cancel();
+    QString errorMsg = "Process error: ";
+    switch (error) {
+        case QProcess::FailedToStart: errorMsg += "Failed to start"; break;
+        case QProcess::Crashed: errorMsg += "Crashed"; break;
+        case QProcess::Timedout: errorMsg += "Timed out"; break;
+        case QProcess::WriteError: errorMsg += "Write error"; break;
+        case QProcess::ReadError: errorMsg += "Read error"; break;
+        default: errorMsg += "Unknown error"; break;
+    }
+    errorMsg += "\n" + m_createProcess->errorString();
+
+    QMessageBox::critical(this, "Error", errorMsg);
+    m_createProcess->deleteLater();
+    m_createProcess = nullptr;
+}
+
+void CreateContainerDialog::handleCreateFinished(int exitCode, QProcess::ExitStatus exitStatus)
+{
+    m_progressDialog->cancel();
+
+    if (exitStatus == QProcess::NormalExit && exitCode == 0) {
+        QString successMsg = QString("Container '%1' created successfully!").arg(containerName());
+        m_progressDialog->setLabelText(successMsg);
+        QMessageBox::information(this, "Success", successMsg);
+        m_backend->enterContainer(containerName());
+        accept();
+    } else {
+        QString errorOutput = QString::fromUtf8(m_createProcess->readAll());
+        QString errorMsg = QString("Container creation failed (exit code: %1)\n").arg(exitCode);
+        if (!errorOutput.isEmpty()) {
+            errorMsg += "\nError output:\n" + errorOutput;
+        }
+        QMessageBox::critical(this, "Error", errorMsg);
+    }
+
+    m_createProcess->deleteLater();
+    m_createProcess = nullptr;
+}
+
+QString CreateContainerDialog::containerName() const
+{
+    return m_nameEdit->text().trimmed();
+}
 
 QString CreateContainerDialog::imageUrl() const
 {
@@ -95,9 +231,15 @@ QString CreateContainerDialog::imageUrl() const
     return QString();
 }
 
-QString CreateContainerDialog::homePath() const { return m_homeEdit->text(); }
+QString CreateContainerDialog::homePath() const
+{
+    return m_homeEdit->text().trimmed();
+}
 
-bool CreateContainerDialog::useInit() const { return m_initCheckbox->isChecked(); }
+bool CreateContainerDialog::useInit() const
+{
+    return m_initCheckbox->isChecked();
+}
 
 QStringList CreateContainerDialog::volumes() const
 {
