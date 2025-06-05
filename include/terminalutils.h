527ec0d57b5fdfaa8fd8194cd5dca15459418827
Hadi Chokr <hadichokr@icloud.com>
2025-06-05 13:22:24 +0200

    Add more Terminals

--- include/terminalutils.h
+++ include/terminalutils.h
@@ -4,9 +4,21 @@
 
 inline QMap<QString, QString> getTerminalIconMap() {
     return {
-        {"gnome-terminal", "gnome-terminal"},
-        {"konsole", "utilities-terminal"},
-        {"xfce4-terminal", "xfce4-terminal"},
-        {"xterm", "xterm"}
+    {"gnome-terminal", "gnome-terminal"},
+    {"xfce4-terminal", "xfce4-terminal"},
+    {"xterm", "xterm"},
+    {"konsole", "utilities-terminal"},
+    {"org.kde.konsole", "utilities-terminal"}, // Flatpak version
+    {"kgx", "kgx"}, // GNOME Console
+    {"tilix", "tilix"},
+    {"alacritty", "alacritty"},
+    {"kitty", "kitty"},
+    {"terminator", "terminator"},
+    {"urxvt", "urxvt"},
+    {"lxterminal", "lxterminal"},
+    {"eterm", "eterm"},
+    {"st", "st"},
+    {"wezterm", "wezterm"},
+    {"konsole.wrapper", "utilities-terminal"}
     };
 }
