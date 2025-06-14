9eb70db409be53ec88949d44f6907de45dc4dbf7
Hadi Chokr <hadichokr@icloud.com>
2025-06-14 12:20:03 +0200

    I18n

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[])
 {
-    KLocalizedString::setApplicationDomain("Kontainer");
+    KLocalizedString::setApplicationDomain("kontainer");
 
     QApplication app(argc, argv);
 
